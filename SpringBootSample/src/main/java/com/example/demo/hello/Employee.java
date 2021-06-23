5cae0cf4430139d06ec1f80b0c4780a503aec8cc
te180636 <84059840+te180636@users.noreply.github.com>
2021-06-23 15:43:22 +0900

    Delete SpringBootSample directory

--- SpringBootSample/src/main/java/com/example/demo/hello/Employee.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.hello;
-import lombok. Data;
-
-@Data
-public class Employee {
-	private String employeed;
-	private String employeedName;
-	private int employeeAge;
-}
--- SpringBootSample/src/main/resources/templates/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO employee (id, name, age)
-VALUES(' 1', 'Tom', 30);
